a429e1247216be64feb225a11b8dd7647a2b12af
pairmai <pairmai_p27@hotmail.com>
2025-05-28 21:21:27 +0700

    edit color event

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,10 +4,9 @@
 //   const { locale } = await params; // ใช้ params ที่ส่งมาโดยตรง
 //   return redirect(`/${locale}/login`); // ใช้ locale ที่ถูกต้อง
 // }
-
 import { redirect } from "next/navigation";
-
-export default function LocalePage({ params }: { params: { locale: string } }) {
-  const { locale } = params; // ไม่ต้อง await
-  return redirect(`/${locale}/login`);
+ 
+export default async function LocalePage({ params }: { params: { locale: string } }) {
+  const { locale } = await params; // ใช้ params ที่ส่งมาโดยตรง
+  return redirect(`/${locale}/login`); // ใช้ locale ที่ถูกต้อง
 }
